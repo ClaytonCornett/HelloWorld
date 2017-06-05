570698bcf827685fa986bb50beda6179988f98c6
ClaytonCornett <cscyrd@mail.missouri.edu>
2017-06-05 17:15:32 -0500

    Initialized Hello World in Xcode

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by MU IT Program on 6/5/17.
+//  Copyright © 2017 ClaytonCornett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
